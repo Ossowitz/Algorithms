59a0bec94bdef7e54c29f36470b2a60a80a68627
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-10-03 01:53:56 +0300

    Scan the person's choice

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
 
 int main() {
+    int choice;
+    do {
+        printf("\t1. Play with a friend\n\t"
+               "2. Play with bot\n\t"
+               "3. Scoreboard\n\t"
+               "4. Exit\n"
+        );
+        scanf("%d", &choice);
+        getchar();
+        system("cls");
+    } while (choice != 4);
     return 0;
 }
