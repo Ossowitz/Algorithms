9f2d10852fd828dd47cdd8709d00695d5e7674c0
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-10-04 07:21:29 +0300

    Init structs and global vars

--- main.c
+++ main.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
+typedef struct User {
+    char name[100], str_coins[10];
+    int coins, tmp_coins, missile;
+    bool winner;
+} User;
 
+typedef struct Ship {
+    int length, width;
+} Ship;
+
+typedef struct Coord {
+    int x, y;
+} Coord;
+
+typedef struct Node {
+    Ship info;
+    Coord head, tail;
+    int hit;
+    struct Node *next;
+} Node;
+
+typedef struct Playback {
+    char **map;
+    struct Playback *next;
+} Playback;
+
+// Init
+Playback *playback;
+Ship ships[30];
+User users[2];
+int maprow = 10, mapcol = 10;
+int numOfTotalShips = 10, largest_ship_area = 5;
 
 int main() {
     int choice;
